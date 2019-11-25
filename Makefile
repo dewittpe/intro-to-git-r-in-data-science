47743662f9dee92a05ec8451a200cdfbcf266f1e
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2019-11-25 14:44:41 -0700

    wip

--- Makefile
+++ Makefile
@@ -2,8 +2,13 @@
 
 all : slides/intro-to-git.pdf
 
-slides/intro-to-git.pdf : slides/intro-to-git.tex slides/overview.tex
-	latexmk $< --pdf --outdir=slides
+slides/intro-to-git.pdf : slides/intro-to-git.tex slides/overview.tex eglog.log egdiff
+	latexmk $< --pdf --outdir=slides --shell-escape
+
+eglog.log egdiff : make-eg-git-log.sh
+	./make-eg-git-log.sh
+	git --git-dir=git-example-log/.git lg3 > eglog.log
+	git --git-dir=git-example-log/.git diff HEAD~4 HEAD~2 > egdiff
 
 clean :
 	latexmk -C slides/intro-to-git.tex --outdir=slides
--- make-eg-git-log.sh
+++ make-eg-git-log.sh
@@ -12,45 +12,54 @@ touch analysis.R
 git init
 
 git add data.dat analysis.R
-git commit --date="2010-05-28:15:37" -m "my test data and analysis"
+git commit --no-gpg-sign --date="2010-05-28:15:37" -m "my test data and analysis"
 
 echo "change 1" >> data.dat
 
-git commit --date="2010-05-28:16:29" -am "re-test"
+git commit --no-gpg-sign --date="2010-05-28:16:29" -am "re-test"
 
 echo "change 2" >> data.dat
-git commit --date="2010-05-28:17:43" -am "re-re-test"
+git commit --no-gpg-sign --date="2010-05-28:17:43" -am "re-re-test"
 
 echo "change 3" >> data.dat
-git commit --date="2010-05-28:19:17" -am "callibrate"
+git commit --no-gpg-sign --date="2010-05-28:19:17" -am "callibrate"
 
 echo "data go bye bye" > data.dat
-git commit --date="2010-05-28:19:20" -am "huh?"
+git commit --no-gpg-sign --date="2010-05-28:19:20" -am "huh?"
 
 echo "YOU SHALL NOT PASS!" > data.dat
-git commit --date="2010-05-28:21:58" -am "WTF"
+git commit --no-gpg-sign --date="2010-05-28:21:58" -am "WTF"
 
 echo "data is back, but what is it?" > data.dat
-git commit --date="2010-05-29:00:37" -am "aaarrgh"
+git commit --no-gpg-sign --date="2010-05-29:00:37" -am "aaarrgh"
 
 echo "data is back, but what is it?" >> data.dat
-git commit --date="2010-05-29:02:40" -am "#\$@*&!!"
+git commit --no-gpg-sign --date="2010-05-29:02:40" -am "#\$@*&!!"
 
 echo "more bad data" >> data.dat
-git commit --date="2010-05-29:03:22" -am "crap"
+git commit --no-gpg-sign --date="2010-05-29:03:22" -am "crap"
 
 echo "1,2,3" > data.dat
-git commit --date="2010-05-29:04:16" -am "notbad"
+git commit --no-gpg-sign --date="2010-05-29:04:16" -am "notbad"
 
 echo "5,6,7" >> data.dat
-git commit --date="2010-05-29:04:47" -am "woohoo!!"
+git commit --no-gpg-sign --date="2010-05-29:04:47" -am "woohoo!!"
 
 echo "1,1,1,2,2,3,4,5,7,9,16,..." >> data.dat
-git commit --date="2010-05-29:04:47" -am "USETHISONE"
+git commit --no-gpg-sign --date="2010-05-29:04:47" -am "USETHISONE"
+git tag -a "v0.1.0" -m 'use this data set, show to prof smith'
 
 touch notes_metting_with_prof_smith.txt
 git add notes_metting_with_prof_smith.txt
-git commit --date="2010-05-29:11:38" -m "Add notes from meeting with Prof Smith"
+git commit --no-gpg-sign --date="2010-05-29:11:38" -m "Add notes from meeting with Prof Smith"
 
 echo "" >> data.dat
-git commit --date="2010-05-30:08:37" -am "Starting over"
+git commit --no-gpg-sign --date="2010-05-30:08:37" -am "Starting over"
+
+git checkout HEAD~1
+git checkout -b startover-again
+echo "good data" > data.dat
+git commit --no-gpg-sign --date="2010-05-30:09:14" -am "Starting over, again"
+git checkout master
+git merge --no-gpg-sign --no-edit --strategy=recursive -Xtheirs startover-again
+
